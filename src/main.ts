8dcd22e60befde714a3063d969fa193be7813864
Blaž Vilar <blaz.vilar12@gmail.com>
2025-06-13 16:27:02 +0200

    feat: Add primevue package

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,17 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+
 import App from './App.vue'
 
 const app = createApp(App)
-
 app.use(createPinia())
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+})
 
 app.mount('#app')
