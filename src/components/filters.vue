a2c36429d02de7a203c4985ca9d0be0b0c7d65f7
Blaž Vilar <blaz.vilar12@gmail.com>
2025-06-16 14:40:39 +0200

    chore: cleanup unused component

--- src/components/filters.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-defineOptions({
-  name: 'AppFilters',
-})
-</script>
-
-<template>
-  <div class="flex items-center gap-2"></div>
-</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import AppFilters from '@/components/filters.vue'
 import AppTable from '@/components/table.vue'
 </script>
 
@@ -8,7 +7,6 @@ import AppTable from '@/components/table.vue'
     class="flex h-[calc(100vh-56px)] w-full shrink-0 items-center overflow-hidden rounded-t-2xl bg-zinc-200 dark:bg-zinc-800"
   >
     <main class="h-full w-full grow overflow-y-auto">
-      <AppFilters />
       <AppTable />
     </main>
 
