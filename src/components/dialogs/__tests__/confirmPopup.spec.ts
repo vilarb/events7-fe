380d31d989fcdb166ab3b5281a62bf92ecbf5c23
Blaž Vilar <blaz.vilar12@gmail.com>
2025-06-19 14:54:35 +0200

    bug: fix typo in test file

--- src/components/dialogs/__tests__/confirmPopup.spec.ts
+++ src/components/dialogs/__tests__/confirmPopup.spec.ts
@@ -44,15 +44,15 @@ describe('ConfirmPopup', () => {
   })
 
   it('updates visible prop when cancel button is clicked', async () => {
-    const cancel-button = wrapper.find('[data-test-id=\"cancel-button\"]')
+    const cancelButton = wrapper.find('[data-test-id=\"cancel-button\"]')
 
     // Check that the cancel button is rendered
-    expect(cancel-button.text()).toContain('Test cancel')
+    expect(cancelButton.text()).toContain('Test cancel')
 
     // Set cancelLabel prop to undefined and check that the default cancel label is rendered
     await wrapper.setProps({ cancelLabel: undefined })
     await wrapper.vm.$nextTick()
-    expect(cancel-button.text()).toContain('Cancel')
+    expect(cancelButton.text()).toContain('Cancel')
 
     // Click the cancel button and check that the visible prop is updated to false
     await wrapper.find('[data-test-id=\"cancel-button\"]').trigger('click')
@@ -62,18 +62,18 @@ describe('ConfirmPopup', () => {
   })
 
   it('emits confirm event when confirm button is clicked and renders correct label', async () => {
-    const confirm-button = wrapper.find('[data-test-id=\"confirm-button\"]')
+    const confirmButton = wrapper.find('[data-test-id=\"confirm-button\"]')
 
     // Check that the confirm button is rendered
-    expect(confirm-button.text()).toContain('Test confirm')
+    expect(confirmButton.text()).toContain('Test confirm')
 
     // Set confirmLabel prop to undefined and check that the default confirm label is rendered
     await wrapper.setProps({ confirmLabel: undefined })
     await wrapper.vm.$nextTick()
-    expect(confirm-button.text()).toContain('Confirm')
+    expect(confirmButton.text()).toContain('Confirm')
 
     // Click the confirm button and check that the confirm event is emitted
-    await confirm-button.trigger('click')
+    await confirmButton.trigger('click')
 
     expect(wrapper.emitted('confirm')).toBeTruthy()
     expect(wrapper.emitted('confirm')?.[0]).toEqual([])
